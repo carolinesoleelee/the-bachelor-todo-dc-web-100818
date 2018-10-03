76972bb8bb0862ab808433dcdc313f1c9a1d24c4
Solee Lee <carolinesoleelee@gmail.com>
2018-10-03 03:46:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -30,8 +30,17 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
-end
+  def get_contestant_name(data, occupation)
+    data.each do |season, contestants|
+      contestants.each do |contestant|
+        contestant.each do |key, value|
+          if occupation == value
+            return contestant["name"]
+          end
+        end
+      end
+    end
+  end
 
 def get_average_age_for_season(data, season)
   # code here
